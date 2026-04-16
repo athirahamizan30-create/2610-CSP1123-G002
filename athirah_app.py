0ec0be4bff5c986d8781b0898b1ce4a2295087ee
Nurul Athirah <athirahamizan30@gmail.com>
2026-04-16 20:35:46 +0800

    create new route

--- athirah_app.py
+++ athirah_app.py
@@ -2,6 +2,19 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
+def create_user_registration():
+    app = Flask(__name__)
+    
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+    
+    return app
+
+
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
