94f07310329ce27db19103e843755453f7b171e8
Nurul Athirah <athirahamizan30@gmail.com>
2026-04-16 21:23:22 +0800

    Creating database table

--- athirah_app.py
+++ athirah_app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin
+from sqlalchemy import text
+
 
 app = Flask(__name__)
 
@@ -8,16 +10,30 @@ db= SQLAlchemy()
 login_manager = LoginManager()
 
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.string(120), unique=True, nullable=False)
+    password_hash = db.Column(db.string(255), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+    
+    
+
+
 def create_user_registration():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'user_registration_athirah'
-    app.config['SQLALCHEMY_DATABSE_URI'] = "mysql+pymysql://athirah:Tiya071!@localhost/my_database"
+    app.config['SQLALCHEMY_DATABSE_URI'] = "mysql+pymysql://athirah:Tiya071!@localhost/CareerTrack_Database"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init.app(app)
     login_manager.init.app(app)
     login_manager.login_view = "login"
+
+    @app.route('/')
 
     @app.route('/')
     def index():
@@ -37,6 +53,9 @@ def create_user_registration():
     @login_manager.user_loader
     def load_user(user_id):
         return None
+    
+    with app.app_context():
+        db.create_all()
     
     
     
