76ed32d1fa2aa10cbbe5477f2c83bed7c65b19bc
syairahani16 <syairahani1.0@gmail.com>
2026-04-22 21:34:19 +0800

    upload file

--- app.py
+++ app.py
@@ -9,10 +9,6 @@ app = Flask(__name__, template_folder="templates")
 def index ():
     return render_template("document.html")
 
-#go to other page/route connected to web, <> is a variable, can be added in function
-@app.route('/redirect_endpoint')
-def redirect_endpoint():
-    return redirect (url_for("test"))
 
 #run app with local host (0.0.0.0) and port 5555, set debug to true, does not have to reset app everytime theres error
 if __name__ == "__main__":
