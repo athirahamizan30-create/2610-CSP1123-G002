aaed84ef0dc01d3c84876bf23883d1698a7c28c7
Nurin Jazmienna <nurinjazmienna@gmail.com>
2026-05-16 13:09:53 +0800

    Fixed some error

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import mysql.connector
 import re
 import uuid
 import os
+import secrets
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
