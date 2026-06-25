e31ed9a2f4e4d21eb3b32cddb023602747c19ebc
Nurul Athirah <athirahamizan30@gmail.com>
2026-06-26 01:05:37 +0800

    fix email

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from dotenv import load_dotenv
 from zoneinfo import ZoneInfo
 
-app = Flask(__name__, template_folder="templates", static_folder="static/uploads")
+
 db= SQLAlchemy()
 mail = Mail()
 login_manager = LoginManager()
@@ -28,22 +28,6 @@ bcrypt = Bcrypt()
 socketio = SocketIO()
 load_dotenv()
 
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['SECRET_KEY'] = 'secretley'
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://athirah:Tiya071!@localhost/CareerTrack_Database"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
-app.config['REMEMBER_COOKIE_DURATION'] = timedelta(days=15)
-
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-
-profile_pics_folder = os.path.join(
-    app.config['UPLOAD_FOLDER'],
-    'profile_pics'
-)
-
-os.makedirs(profile_pics_folder, exist_ok=True)
 
 logger = logging.getLogger(__name__)
 login_manager.login_view = "login"
@@ -302,29 +286,34 @@ def send_reminders(app):
 def create_app():
 
     app = Flask(__name__)
+    load_dotenv()
+
     bcrypt.init_app(app)
     app.config.from_object(Config)
     db.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = "login"
-    scheduler = BackgroundScheduler()
-    scheduler.start()
 
     app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-    app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+    app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
     app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
     app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL') == 'True'
     app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
     app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
+
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secretley')
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://athirah:Tiya071!@localhost/CareerTrack_Database"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['REMEMBER_COOKIE_DURATION'] = timedelta(days=15)
     app.config['UPLOAD_FOLDER'] = 'static/uploads'
     app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
 
-
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
+    
+    profile_pics_folder = os.path.join(app.config['UPLOAD_FOLDER'],'profile_pics')
+    os.makedirs(profile_pics_folder, exist_ok=True)
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     
     mail.init_app(app)
 
@@ -546,16 +535,6 @@ def create_app():
 
         return redirect(url_for('dashboard'))
 
-    def send_email_job(app, msg):
-        with app.app_context():
-            try:
-                mail.send(msg)
-                print("Email sent successfully to:", msg.recipients)
-            except Exception as e:
-                print("Email error:", e)
-
-
-
     @app.route('/forgot_password', methods=['POST', 'GET'])
     def forgot_password():
 
@@ -570,7 +549,7 @@ def create_app():
                 flash("No user with that email found", "error")
                 return redirect(url_for("forgot_password"))
             
-            PasswordResetId.query.filter_by(user_id=user.id).delete()
+            user.password_reset_ids.clear()
 
             new_password_reset_id = PasswordResetId(user_id=user.id)
             db.session.add(new_password_reset_id)
@@ -585,10 +564,8 @@ def create_app():
                 recipients = [email],
                 body = f"Reset your password using the link below\n\n{password_reset_link}"
             )
-
-            msg.sender = app.config['MAIL_USERNAME']
             try:
-                scheduler.add_job(send_email_job, args=[app, msg])
+                mail.send(msg)
 
                 context = {
                     "reset_sent": True,
