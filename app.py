77d6d05c65d1239deec4ae24eca6794fc6e3a0cd
Nurul Athirah <athirahamizan30@gmail.com>
2026-06-30 20:27:39 +0800

    fix code

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 from apscheduler.schedulers.background import BackgroundScheduler
 from dotenv import load_dotenv
 from zoneinfo import ZoneInfo
-import threading
 from collections import defaultdict
 import resend
 from decouple import config
@@ -1328,4 +1327,3 @@ if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=port, debug=False)
 
 
-#test bug kat dashboard, kat rememebr me login user, jgn lupe delete, logout pon ada 
